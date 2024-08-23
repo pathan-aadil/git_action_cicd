b0775d62ce5dfa632769d3fca4e82b5049cd2e39
pathan-aadil <93585802+pathan-aadil@users.noreply.github.com>
2024-08-23 19:42:47 +0530

    added

--- app.py
+++ app.py
@@ -1,12 +1,2 @@
 import streamlit as st
-import pandas as pd
-
-
-data = {
-            'Course Name': ['DevOps', 'Azure2'], 
-            'Duration': ['30 days', '30days']
-    }
-df = pd.DataFrame(data)
-
-
-st.dataframe(df)
+st.write("cicd pipline using github action")
